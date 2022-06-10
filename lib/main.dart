cc65da905f86eb2b60c81fac158212a84cc0e94d
Juwon-Yun <eooll2@daum.net>
2022-06-11 00:32:33 +0900

    Feat: bottom_sheet, viewInsets, isScrollControlled

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_calendar/screen/main_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-void main()  {
+void main() {
   // flutter 프레임웤이 준비될떄까지 기다릴 수 있다.
   WidgetsFlutterBinding.ensureInitialized();
 
   initController();
 
-  runApp(const MainScreen());
+  runApp(
+      MaterialApp(
+        theme: ThemeData(fontFamily: 'NotoSans'),
+        debugShowCheckedModeBanner: false,
+        home: MainScreen()
+      )
+  );
 }
 
 initController() async {
   await initializeDateFormatting();
 }
-
-
--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar/components/calendar.dart';
+import 'package:flutter_calendar/components/new_schedule_bottom_sheet.dart';
 import 'package:flutter_calendar/components/schedule_card.dart';
 import 'package:flutter_calendar/components/today_banner.dart';
+import 'package:flutter_calendar/constants/colors.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -20,42 +22,28 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(fontFamily: 'NotoSans'),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              Calendar(
-                selectedDay: selectedDay,
-                focusedDay: focusedDay,
-                onDaySelected: _onDaySelected,
-              ),
-              const SizedBox(height: 8),
-              TodayBanner(
-                scheduleCount: 1,
-                selectedDay: selectedDay,
-              ),
-              SizedBox(height: 8),
-              Expanded(
-                child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                    child: ListView.separated(
-                        separatorBuilder: (context, index)=>SizedBox(height: 8),
-                        itemCount: 190,
-                        itemBuilder: (context, index) {
-                          return ScheduleCard(
-                              startTime: 8,
-                              endTime: 9,
-                              content: '프로그래밍 공부하기 $index',
-                              color: Colors.red);
-                        })),
-              ),
-            ],
+          child: Container(
+            child: Column(
+              children: [
+                Calendar(
+                  selectedDay: selectedDay,
+                  focusedDay: focusedDay,
+                  onDaySelected: _onDaySelected,
+                ),
+                const SizedBox(height: 8),
+                TodayBanner(
+                  scheduleCount: 1,
+                  selectedDay: selectedDay,
+                ),
+                SizedBox(height: 8),
+                _ScheduleList(),
+              ],
+            ),
           ),
         ),
-      ),
+        floatingActionButton: renderFloatingActionButton(),
     );
   }
 
@@ -64,5 +52,43 @@ class _MainScreenState extends State<MainScreen> {
       selectedDay = selected;
       focusedDay = selected;
     });
+  }
+
+  FloatingActionButton renderFloatingActionButton() => FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              // 디폴트가 화면의 절반만큼만 사용하는 걸 풀어줌
+              isScrollControlled: true,
+              builder: (_) {
+                return ScheduleBottomSheet();
+              });
+        },
+        backgroundColor: primary_color,
+        child: const Icon(Icons.add),
+      );
+}
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          child: ListView.separated(
+              separatorBuilder: (context, index) => SizedBox(height: 8),
+              itemCount: 190,
+              itemBuilder: (context, index) {
+                return ScheduleCard(
+                    startTime: 8,
+                    endTime: 9,
+                    content: '프로그래밍 공부하기 $index',
+                    color: Colors.red);
+              })),
+    );
   }
 }
