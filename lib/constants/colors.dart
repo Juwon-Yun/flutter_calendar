218a88b3eb4d6205562456cf7a25f182a0ec8012
Juwon-Yun <eooll2@daum.net>
2022-06-13 13:29:47 +0900

    Feat: use select & insert in main.dart

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
 const primary_color = Color(0xFF0DB2B2);
+
+const default_colors = [
+  // 빨강
+  'F44336',
+  // 주황
+  'FF9800',
+  // 노랑
+  'FFEB3B',
+  // 초록
+  'FCAF50',
+  // 파랑
+  '2196F3',
+  // 남색
+  '3F51B5',
+  // 보라
+  '9C27B0',
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar/constants/colors.dart';
+import 'package:flutter_calendar/repository/init_db.dart';
 import 'package:flutter_calendar/screen/main_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -8,15 +11,30 @@ void main() {
 
   initController();
 
-  runApp(
-      MaterialApp(
-        theme: ThemeData(fontFamily: 'NotoSans'),
-        debugShowCheckedModeBanner: false,
-        home: MainScreen()
-      )
-  );
+  runApp(MaterialApp(
+      theme: ThemeData(fontFamily: 'NotoSans'),
+      debugShowCheckedModeBanner: false,
+      home: MainScreen()));
 }
 
 initController() async {
   await initializeDateFormatting();
+  final database = LocalDataBase();
+  final colors = await database.getCategoryColors();
+
+  if (colors.isEmpty) {
+    for (String hexcode in default_colors) {
+      await database.createCategoryColor(
+          // CategoryColorsCompanion(id: , hexCode: );
+          CategoryColorsCompanion(hexCode: Value(hexcode)));
+    }
+  }
+  // [CategoryColor(id: 1, hexCode: F44336),
+  // CategoryColor(id: 2, hexCode: FF9800),
+  // CategoryColor(id: 3, hexCode: FFEB3B),
+  // CategoryColor(id: 4, hexCode: FCAF50),
+  // CategoryColor(id: 5, hexCode: 2196F3),
+  // CategoryColor(id: 6, hexCode: 3F51B5),
+  // CategoryColor(id: 7, hexCode: 9C27B0)]
+  print(await database.getCategoryColors());
 }
